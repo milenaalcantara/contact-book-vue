69780b9a2c9eec2fee93438b5431a15f73a52089
Milena <milena.alcantara@irede.org.br>
2024-08-09 09:08:52 -0300

    add contact list router

---
+++ src/views/ContactListView.vue
@@ -0,0 +1,11 @@
+<template>
+  Contact List
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ContactListView',
+});
+</script>
